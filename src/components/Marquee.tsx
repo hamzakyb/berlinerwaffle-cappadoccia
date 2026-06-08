4ae2c8a0eeaa771b7fec5d083d94203b200512d5
Hamza Koybasi <hamzakybsi@gmail.com>
2026-06-09 01:14:00 +0300

    Use filled stars and hearts in marquee instead of logo

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -6,13 +6,15 @@ export default function Marquee() {
       <span className="text-honey-glaze font-black text-[36px] sm:text-[48px] md:text-[60px] uppercase">Belçika Çikolatası</span>
       <span className="material-symbols-outlined text-honey-glaze text-[36px] sm:text-[48px] md:text-[60px] icon-fill">favorite</span>
       <span className="text-tertiary-container font-black text-[36px] sm:text-[48px] md:text-[60px] uppercase text-outline">Taptaze Meyveler</span>
-      <img src="/logowaffle.png" alt="" className="w-32 h-32 sm:w-40 sm:h-40 object-contain shrink-0 rotate-12 -mx-2" />
-      <span className="text-white font-black text-[36px] sm:text-[48px] md:text-[60px] uppercase">Berliner Waffle Cappadocia</span>
       <span className="material-symbols-outlined text-strawberry-pop text-[36px] sm:text-[48px] md:text-[60px] icon-fill">star</span>
-      <span className="text-strawberry-pop font-black text-[36px] sm:text-[48px] md:text-[60px] uppercase text-outline">Sınırsız Topping</span>
+      <span className="text-white font-black text-[36px] sm:text-[48px] md:text-[60px] uppercase">Berliner Waffle Cappadocia</span>
       <span className="material-symbols-outlined text-tertiary-container text-[36px] sm:text-[48px] md:text-[60px] icon-fill">favorite</span>
+      <span className="text-strawberry-pop font-black text-[36px] sm:text-[48px] md:text-[60px] uppercase text-outline">Sınırsız Topping</span>
+      <span className="material-symbols-outlined text-honey-glaze text-[36px] sm:text-[48px] md:text-[60px] icon-fill">star</span>
       <span className="text-honey-glaze font-black text-[36px] sm:text-[48px] md:text-[60px] uppercase text-outline">Günlük Taze Hamur</span>
+      <span className="material-symbols-outlined text-strawberry-pop text-[36px] sm:text-[48px] md:text-[60px] icon-fill">favorite</span>
       <span className="text-white font-black text-[36px] sm:text-[48px] md:text-[60px] uppercase">Mutluluk Kutusu</span>
+      <span className="material-symbols-outlined text-tertiary-container text-[36px] sm:text-[48px] md:text-[60px] icon-fill">star</span>
     </div>
   );
 
